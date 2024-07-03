c50558ff39e153853a037ad2ea0aab0c08b27578
cuong nm <cuongnm@MacBook-Pro-cua-cuong.local>
2024-07-03 15:12:48 +0700

    update url

--- udagram-frontend/src/environments/environment.ts
+++ udagram-frontend/src/environments/environment.ts
@@ -5,7 +5,7 @@
 export const environment = {
   production: false,
   appName: 'Udagram',
-  apiHost: 'http://localhost:8080/api/v0'
+  apiHost: 'http://aba8e684780b748c08b3a6598091f808-330901272.us-east-1.elb.amazonaws.com:8080/api/v0'
 };
 
 /*
